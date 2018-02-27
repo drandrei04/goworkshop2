3bea6a7cfb334519dcaa7d61ab6cfb989cc68381
Alexandru Grigor <alex.grigor92@gmail.com>
2018-02-27 03:37:02 -0800

    step5

--- main/main.go
+++ main/main.go
@@ -1,22 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goworkshop/importer"
+)
 
 func main() {
-
-	fmt.Println("hello world!")
-	fmt.Println("My name is Claudia")
-	fmt.Println("Razvan Farte was here")
-	fmt.Println("my name is Ioan")
-	fmt.Println("Hello my name is Alex")
-	fmt.Println("And I want to merge my changes")
-	fmt.Println("hello my name is alin")
-	fmt.Println("Hello my nombre es Bogdan")
-	fmt.Println("my name is Ioan")
-	fmt.Println("salut lumeee!!!... de la Radu Dragan")
-	fmt.Println("Hello my name is Florin!")
-	fmt.Println("salut!")
-	fmt.Println("Hello, my name is Bogdan!")
-	fmt.Println("Hello my name is Eduard")
-	fmt.Println("Hello, my name is Andrei")
+	var authors = importer.ImportAuthors()
+	fmt.Printf("Imported authors are: %s\n", authors)
+	var books = importer.ImportBooks()
+	fmt.Printf("Imported books are: %s\n", books)
 }
