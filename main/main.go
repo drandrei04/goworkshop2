46e6b5325e131163e2bfe3c2bd5c1ab4cafc0950
Alexandru Grigor <alex.grigor92@gmail.com>
2018-03-01 18:57:56 +0200

    implemented simple rest enpdoints

--- main/main.go
+++ main/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"goworkshop/model"
 	"goworkshop/importer"
+	"goworkshop/web"
 )
 
 func main() {
 	model.Authors = importer.ImportAuthors("importer/authors.json")
 	fmt.Printf("Imported authors are: %s\n", model.Authors)
 
-	model.Books= importer.ImportBooks("importer/books.json")
+	model.Books = importer.ImportBooks("importer/books.json")
 	fmt.Printf("Imported books are: %s\n", model.Books)
+
+	web.StartWebServer()
 }
